cefa7bfb6d6eae724bdde2b506e494d483d96fdd
Michael Kramer <michaelrkramer@gmail.com>
2025-05-27 18:40:28 -0400

    remove src dir

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  appDirectory: "src",
+  //appDirectory: "src",
   ssr: false,
   buildDirectory: "dist",
   basename: "/website-2/",
