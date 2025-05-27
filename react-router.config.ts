ffed9318141960dba84dde781a2f26b323adde34
Michael Kramer <michaelrkramer@gmail.com>
2025-05-27 18:33:10 -0400

    prerender removed

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
   ssr: false,
-  prerender: true,
   buildDirectory: "dist",
   basename: "/website-2/",
 } satisfies Config;
