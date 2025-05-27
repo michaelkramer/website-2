bcd72d4cd8bea0f6a5590e06d22786d67a8c0f91
Michael Kramer <michaelrkramer@gmail.com>
2025-05-27 17:57:55 -0400

    prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,19 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
 
-  ssr: true,
+  ssr: false,
   buildDirectory: "dist",
   basename: "/website-2/",
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return [
+      "/",
+      "/rls",
+      "/resume",
+      "/icons",
+      "/notes",
+      "/notes/grpccode",
+      "/notes/git",
+    ];
+  },
 } satisfies Config;
