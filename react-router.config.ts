3082911912dfcded644836357fd17c456c11c31a
Michael Kramer <michaelrkramer@gmail.com>
2025-06-03 14:17:06 -0400

    test 9a

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "./src",
   ssr: false,
-  prerender: true,
+  prerender: ["/resume", "/website-2/rls"],
 } satisfies Config;
