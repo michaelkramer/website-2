1c90b5fce3ca83e00a8a37e607baaada448ec132
Michael Kramer <michaelrkramer@gmail.com>
2025-06-02 21:18:44 -0400

    test 5 g

--- src/root.tsx
+++ src/root.tsx
@@ -9,7 +9,27 @@ import {
 import "@fontsource-variable/space-grotesk";
 import "./index.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+// export function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <meta charSet="UTF-8" />
+//         <link rel="icon" type="image/svg+xml" href="/assets/me.svg" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <title>Michael Kramer</title>
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body>
+//         {children}
+//         <ScrollRestoration />
+//         <Scripts />
+//       </body>
+//     </html>
+//   );
+// }
+
+export default function App() {
   return (
     <html lang="en">
       <head>
@@ -21,16 +41,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
 
 export function HydrateFallback() {
