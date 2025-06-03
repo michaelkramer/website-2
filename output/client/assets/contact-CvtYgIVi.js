4c53ba7dadf9d29dd7c8957370ad4c44a560489e
Michael Kramer <michaelrkramer@gmail.com>
2025-06-03 14:13:54 -0400

    test 9

--- output/client/assets/contact-CvtYgIVi.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./with-props-DjNaPOT0.js";import{o}from"./chunk-DQRVZFIR-7KfhYImo.js";const a=t(function(){return o.jsx("div",{children:"Contact"})});export{a as default};
--- react-router.config.ts
+++ react-router.config.ts
@@ -3,15 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "./src",
   ssr: false,
-  // prerender: [
-  //   "/",
-  //   "/contact",
-  //   "/resume",
-  //   "/icons",
-  //   "/rls",
-  //   "/notes",
-  //   "/notes/grpccode",
-  //   "/notes/git",
-  // ],
-  buildDirectory: "./output",
+  prerender: true,
 } satisfies Config;
