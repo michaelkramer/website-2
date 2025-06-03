fe18bb7f3d8ee734d40aa07b3c3470aa7f898079
Michael Kramer <michaelrkramer@gmail.com>
2025-06-02 21:28:46 -0400

    test 5 h

--- src/root.tsx
+++ src/root.tsx
@@ -9,27 +9,7 @@ import {
 import "@fontsource-variable/space-grotesk";
 import "./index.css";
 
-// export function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="UTF-8" />
-//         <link rel="icon" type="image/svg+xml" href="/assets/me.svg" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//         <title>Michael Kramer</title>
-//         <Meta />
-//         <Links />
-//       </head>
-//       <body>
-//         {children}
-//         <ScrollRestoration />
-//         <Scripts />
-//       </body>
-//     </html>
-//   );
-// }
-
-export default function App() {
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -41,12 +21,16 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
 
 export function HydrateFallback() {
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,21 @@
 import {
   type RouteConfig,
   route,
-  layout,
   index,
   // eslint-disable-next-line import/no-unresolved
 } from "@react-router/dev/routes";
 
 export default [
-  layout("./layout/main-layout.tsx", [
-    index("./pages/home.tsx"),
-    route("/contact", "./pages/contact.tsx"),
-    route("/resume", "./pages/resume.tsx"),
-    // route("./icons", "./pages/icons.tsx"),
-    route("/rls", "./pages/rls.tsx"),
-    // route("notes", "./pages/notes.tsx", [
-    //   index("./pages/notes/index.tsx"),
-    //   route("grpccode", "./pages/notes/grpccode.tsx"),
-    //   route("git", "./pages/notes/git.tsx"),
-    // ]),
-  ]),
+  //layout("./layout/main-layout.tsx", [
+  index("./pages/home.tsx"),
+  route("/contact", "./pages/contact.tsx"),
+  route("/resume", "./pages/resume.tsx"),
+  // route("./icons", "./pages/icons.tsx"),
+  route("/rls", "./pages/rls.tsx"),
+  // route("notes", "./pages/notes.tsx", [
+  //   index("./pages/notes/index.tsx"),
+  //   route("grpccode", "./pages/notes/grpccode.tsx"),
+  //   route("git", "./pages/notes/git.tsx"),
+  // ]),
+  //]),
 ] satisfies RouteConfig;
