62206c6d8906298f8ad37e95788a261117d9387b
Michael Kramer <michaelrkramer@gmail.com>
2025-06-06 13:33:17 -0400

    Fixing the Me

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router";
+import me from "../assets/me.svg";
 import Footer from "./footer";
 import { Navbar } from "./navbar";
 import { AppProvider } from "./provider";
+
+export function links() {
+  return [
+    {
+      rel: "icon",
+      href: me,
+      type: "image/svg+xml",
+    },
+  ];
+}
 
 export default function Layout() {
   return (
